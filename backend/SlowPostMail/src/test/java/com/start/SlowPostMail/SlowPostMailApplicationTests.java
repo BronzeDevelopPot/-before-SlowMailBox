567b143bafb86111d9c5270b2e92635fabba259d
[cconac] <[happines862@naver.com]>
2022-03-16 19:03:44 +0900

    springboot

---
+++ backend/SlowPostMail/src/test/java/com/start/SlowPostMail/SlowPostMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start.SlowPostMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowPostMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
