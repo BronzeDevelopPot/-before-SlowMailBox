f41063639c1914c45363d88dac1b8bbdb6af9728
areumH <94692566+areumH@users.noreply.github.com>
2022-03-22 23:26:33 +0900

    box commit

--- frontend/slowmailbox/src/component/main/main.jsx
+++ frontend/slowmailbox/src/component/main/main.jsx
@@ -3,7 +3,21 @@ import "./main.css";
 const Main = () => {
   return (
     <main>
-      <div className="mailbox">이거 우체통임 암튼 그럼</div>
+      <div className="mailbox">
+        <p className="post">P O S T</p>
+
+        <div className="size1"></div>
+        <div className="size1"></div>
+
+        <div className="line">
+          <div className="size2"></div>
+          <div className="wayin"></div>
+        </div>
+
+        <div className="wayout"></div>
+      </div>
+
+      <button className="sendbutton">전송</button>
       <button className="sharebutton">내 우체통 공유하기 (Link 공유)</button>
     </main>
   );
