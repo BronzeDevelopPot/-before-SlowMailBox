d07c318587a416382419d2e26b87d7148ed1e4fa
leeminchae <leemc4019@gmail.com>
2022-03-23 22:23:06 +0900

    first commit

--- frontend/slowmailbox/src/component/login/login.jsx
+++ frontend/slowmailbox/src/component/login/login.jsx
@@ -1,7 +1,11 @@
+import './login.css';
+
 const Login = () => {
     return(
         <login>
-
+            <div>
+                <img className='kakao_login_button' src="/login_img/kakao_login_large_wide.png"></img> 
+            </div>
         </login>
     )
 }
