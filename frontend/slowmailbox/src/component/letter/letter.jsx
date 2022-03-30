f121d773787d38b63881da0986fdc904f9a0292b
leeminchae <leemc4019@gmail.com>
2022-03-31 04:42:02 +0900

    5번째커밋-모달창1css위치조정&이름&폰트

--- frontend/slowmailbox/src/component/letter/letter.jsx
+++ frontend/slowmailbox/src/component/letter/letter.jsx
@@ -14,7 +14,7 @@ const Letter = () => {
 */  
 
   return (
-    <div>
+    <div id='ownglyph'>
       <div>
         <textarea rows="33" cols="40" className="textArea"></textarea>
       </div>
@@ -31,27 +31,13 @@ const Letter = () => {
             <div class ="nickname_ment">닉네임 입력</div>
             <input type="text" class="nickname" name="nickname"></input>
             
-            <div class = "date_content">보낼 날짜를 선택해주세요!</div>
-            <select class="date_year" name="date_year">
-               <option value="2022">2022</option>
-            </select>
-            <div>년</div>
-              <select class="date_month" name="date_month">
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
+            <div class = "date_ment">보낼 날짜를 선택해주세요!</div>
+            <div class = "date_select">
+              <select class="date_year" name="date_year">
+                <option value="2022">2022</option>
               </select>
-              <div>월</div>
-                <select class="date_month" name="date_month">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
+              <div>년</div>
+              <select class="date_month" name="date_month">
                   <option value="4">4</option>
                   <option value="5">5</option>
                   <option value="6">6</option>
@@ -61,30 +47,45 @@ const Letter = () => {
                   <option value="10">10</option>
                   <option value="11">11</option>
                   <option value="12">12</option>
-                  <option value="13">13</option>
-                  <option value="14">14</option>
-                  <option value="15">15</option>
-                  <option value="16">16</option>
-                  <option value="17">17</option>
-                  <option value="18">18</option>
-                  <option value="19">19</option>
-                  <option value="20">20</option>
-                  <option value="21">21</option>
-                  <option value="22">22</option>
-                  <option value="23">23</option>
-                  <option value="24">24</option>
-                  <option value="25">25</option>
-                  <option value="26">26</option>
-                  <option value="27">27</option>
-                  <option value="28">28</option>
-                  <option value="29">29</option>
-                  <option value="30">30</option>
-                  <option value="31">31</option>
-                </select>
+              </select>
+              <div>월</div>
+              <select class="date_month" name="date_month">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+              </select>
               <div>일</div>
+            </div>
 
-
-              <button class="close-area">작성</button>
+              <button class="summit_button">작성</button>
           </div>
         </div>
       </div>
