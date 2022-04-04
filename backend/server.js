76f04f9b65cac315fbfad33cd4a4ee19e2adf216
okxooxoo <vholls2@naver.com>
2022-04-05 05:56:53 +0900

    delete .env

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({extended: true})); // 요청에서 온 데이터를 
 var db; // 몽고디비 연결 ↓
 MongoClient.connect(process.env.DB_URL, function(err, client){
     if (err) return console.log(err);
-    db = client.db('SlowMailBox') // 'SlowMailBox'라는 데이터베이스에 접속
+    db = client.db('SlowMailBox'); // 'SlowMailBox'라는 데이터베이스에 접속
 
     app.listen(process.env.PORT, function() { // 8080포트에 서버 열기
         console.log('listening on 8080');
