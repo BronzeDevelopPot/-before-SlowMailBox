6805e012f4aeb026a8d5feb2e08b52ae7be4ae0c
[cconac] <[happines862@naver.com]>
2022-04-06 02:14:04 +0900

    ADD: kakao login auth url

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const axios = require('axios');
+const qs = require('qs');
+const session = require('express-session');
 const MongoClient = require('mongodb').MongoClient;
 const path = require('path'); // 경로를 쉽게 다루기 위함
 require('dotenv').config(); // 환경변수 사용을 위함
-const app = express();
 app.use(express.urlencoded({extended: true})); // 요청에서 온 데이터를 쉽게 처리하기 위함
+
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express:app,
+})
+
+app.use(session({
+    secret: 'aaa',
+    resave: true,
+    secure: false,
+    saveUninitialized:false,
+}))
+
+const kakao = {
+    clientID: 'env.KAKAO_ID',
+    clientSecret: 'env.Secret',
+    redirectURL: 'env.Redirect_URL' 
+}
+
+app.get('/auth/kakao',(req,res)=>{
+    const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectURL}&response_type=code&scope=profile_nickname,account_email`;
+    res.redirect(kakaoAuthURL);
+})
 
 var db; // 몽고디비 연결 ↓
 MongoClient.connect(process.env.DB_URL, function(err, client){
