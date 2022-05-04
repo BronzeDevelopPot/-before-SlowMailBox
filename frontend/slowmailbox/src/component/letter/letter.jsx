429a63a811c597ce094ee030ec05861d64a0a388
areumH <94692566+areumH@users.noreply.github.com>
2022-05-04 14:39:14 +0900

    letter page commit

--- frontend/slowmailbox/src/component/letter/letter.jsx
+++ frontend/slowmailbox/src/component/letter/letter.jsx
@@ -1,95 +1,40 @@
-import { Link } from "react-router-dom";
+import Modal from "./modal/modal";
+import { React } from "react";
+import { useState } from "react";
 import "./letter.css";
 
 const Letter = () => {
-/*
-    {
-        const modal = document.getElementById("modal");
-        const btnModal = document.getElementById("modalTest");
-        btnModal.addEventListener("click", e =>{
-            modal.style.display = "flex";
-        })
-    }
+  const [modal, setModal] = useState(false);
 
-*/  
+  const onClick = () => {
+    setModal(true);
+  };
+
+  /* textarea 글자 수 제한 */
+  const checkLength = (e, setLength) => {
+    const targetText = e.target.value;
+    const textLength = e.target.value.length;
+
+    if (textLength <= 300) {
+      setLength(e.target.value.length);
+    } else {
+      e.target.value = targetText.substr(0, 300);
+      setLength(196);
+    }
+  };
 
   return (
-    <div id='ownglyph'>
-      <div>
-        <textarea rows="33" cols="40" className="textArea"></textarea>
+    <div id="ownglyph">
+      <div>{modal === true ? <Modal></Modal> : null}</div>
+      <div className="letter">
+        <textarea className="textArea_style" onChange={checkLength}></textarea>
       </div>
+
       <div className="letter_sendline">
-        <button className="letter_send">작성</button>
+        <button className="letter_send" onClick={onClick}>
+          작성
+        </button>
       </div>
-
-
-
-      {/* 모달창 테스트 */}
-      <div id="modal" class="modal-overlay">
-        <div class="modal-window">
-          <div id = "content">
-            <div class ="nickname_ment">닉네임 입력</div>
-            <input type="text" class="nickname" name="nickname"></input>
-            
-            <div class = "date_ment">보낼 날짜를 선택해주세요!</div>
-            <div class = "date_select">
-              <select class="date_year" name="date_year">
-                <option value="2022">2022</option>
-              </select>
-              <div>년</div>
-              <select class="date_month" name="date_month">
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-              </select>
-              <div>월</div>
-              <select class="date_month" name="date_month">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-              </select>
-              <div>일</div>
-            </div>
-
-              <button class="summit_button">작성</button>
-          </div>
-        </div>
-      </div>
-
     </div>
   );
 };
---
+++ frontend/slowmailbox/src/component/letter/modal/modal.jsx
@@ -0,0 +1,15 @@
+import "./modal.css";
+
+const Modal = () => {
+  return (
+    <div id="modal" class="modal-overlay">
+      <div class="modal-window">
+        <div id="content">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
